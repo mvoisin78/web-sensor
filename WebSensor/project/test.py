7d7667fa6255a335d89fa4adb6fa547780e53f43
MartinGLUCP <51168973+MartinGLUCP@users.noreply.github.com>
2020-03-27 17:08:36 +0100

    new test
    création d'une nouvelle fonction

--- WebSensor/project/test.py
+++ WebSensor/project/test.py
@@ -1,7 +1,7 @@
 import spacy
 import string
 import time
-from geotext import GeoText
+#from geotext import GeoText
 
 import pandas as pd
 import numpy as np
@@ -22,7 +22,7 @@ from dbconn import *
 
 nlp = spacy.load("fr_core_news_sm", parse=True, tag=True, entity=True)
 
-text = u'Cest la fete le match de foot au Stade de France hihihi le 27/03/12 nul giroux la girouette'
+#text = u'Cest la fete le match de foot au Stade de France hihihi le 27/03/12 nul giroux la girouette'
 #text = u'Tous les films de Ghibli chez Netflix'
 
 def token(sentence): #Tokenize et retourne les labels
@@ -63,50 +63,51 @@ def create_vector(features,tokens):
     v = [0 for i in range(0,len(features))]
     for i in range(0,len(features)):
         if features[i] in tokens :
+            #if features[i] == 
             v[i]=1
     return v
         
+def vectorization(f) :
+    #Changer pour vous:Hp6ZTa7
+    #connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+    client = MongoClient("mongodb+srv://martin:Hp6ZTa7@tweets-yzhnw.mongodb.net/test?retryWrites=true&w=majority")
+    db=client.admin
+    mydb = client["Tweet"]
+    mycol = mydb["tweets"]
+    i = 0
+    vecteur_count = 0
+
+    ## ATTENTION ##
+    deleteAllVectors()
+    ##           ##
+
+    for t in mycol.find({}):
+        
+        normalize_text = normalize_corpus(t.get('text'))
+        
+        final_text = remove_stopwords(normalize_text)
+        #print(final_text)
+
+        #creation du vecteur
+        v = create_vector(f,final_text.split(" "))
+        #print(v)
+
+    ##################################DEBUG AND INSERT###########################################################################
+        #c est juste pour voir si ya des vecteurs avec des 1 => oui
+        if sum(v) >=1 :
+            insert_vector(v,t.get('text'),t.get('tweet_id'),t.get('date'))
+            vecteur_count +=1
+        i+=1
+        print("nb possibles Event : ",vecteur_count, "/", i,time.perf_counter())
+
 
 #on lemmatize les features pr la proprete
 features = "sport foot stade football athletisme basketball natation cyclisme golf handball equitation judo karate marathon rugby ski taekwondo volleyball superbowl match coupe championnat attentat tremblement concert exposition explosion festival terroriste election fete cirque gala oscar cesar congre forum ceremoni convention spectacle theatr politique trianon vote election accident mort sortie album cinema film coronaviru viru maladie epidemie pandemie nouveau solde carnaval france europe monde national regionnal zenith olympia bataclan hippodrome cinema"
 f = lemmatize_text(features).split(" ")
 
+vectorization(f)
+
 #test API pour la localisation mais osef
 
-places = GeoText("text")
-print(places.cities)
-
-
-#Changer pour vous
-#connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("mongodb+srv://martin:Hp6ZTa7@tweets-yzhnw.mongodb.net/test?retryWrites=true&w=majority")
-
-db=client.admin
-mydb = client["tweetest"]
-mycol = mydb["tweets"]
-i = 0
-vecteur_count = 0
-
-## ATTENTION ##
-deleteAllVectors()
-##           ##
-
-for t in mycol.find({}):
-    
-    normalize_text = normalize_corpus(t.get('text'))
-    
-    final_text = remove_stopwords(normalize_text)
-    #print(final_text)
-
-    #creation du vecteur
-    v = create_vector(f,final_text.split(" "))
-    #print(v)
-
-##################################DEBUG AND INSER###########################################################################
-    #c est juste pour voir si ya des vecteurs avec des 1 => oui
-    if sum(v) >=1 :
-        insert_vector(v,t.get('text'),t.get('tweet_id'),t.get('date'))
-        vecteur_count +=1
-    i+=1
-    print("nb possibles Event : ",vecteur_count, "/", i,time.perf_counter())
-
+#places = GeoText("text")
+#print(places.cities)
