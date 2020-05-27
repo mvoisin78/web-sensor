558774a5fbefa068b5a670f7f3e36f82538c3139
GabrielChev <46599386+GabrielChev@users.noreply.github.com>
2020-05-27 13:17:29 +0200

    Add files via upload

--- Website_sensor/index.php
+++ Website_sensor/index.php
@@ -54,14 +54,97 @@ session_start();
 
 
 <div class="informations-accueil">
-  <p>Ici y aura les infos sur l'event affiché</p>
-  <ul><li>tu peux mettre une liste nom, lieu ...etc</li></ul>
+	<p>Ici y aura les infos sur l'event affiché</p>
+	<ul><li>tu peux mettre une liste nom, lieu ...etc</li></ul>
 </div>
-<div class="accueil-graphique">
-  <p>on verra bien ce que la vie nous reservera</p>
+<div id="piechart" class="accueil-graphique">
+
+<script>
+
+	//PIECHART
+
+	var data_list_obj = [
+		{"label" : "Evenement 1",
+		"occurence" : 2348
+		},
+		{"label" : "Evenement 2",
+		"occurence" : 7851
+		},
+		{"label" : "Evenement 3",
+		"occurence" : 9544
+		},
+		{"label" : "Evenement 2",
+		"occurence" : 5624
+		},
+		{"label" : "Evenement 3",
+		"occurence" : 7544
+		},
+		{"label" : "Evenement 2",
+		"occurence" : 1551
+		},
+		{"label" : "Evenement 3",
+		"occurence" : 444
+		}
+	];
+	occurence_list = [];
+	label_list = [];
+
+	//json to list
+	for (const obj of data_list_obj) {
+		label_list.push(obj.label)
+		occurence_list.push(obj.occurence)
+	}
+
+	console.log(label_list)
+	console.log(occurence_list)
+	var width = document.getElementById('piechart').offsetWidth,
+		height = document.getElementById('piechart').offsetHeight,
+		radius = Math.min(width, height) / 2;
+
+	var color = ["red","orange","yellow","34CA00","green","steelblue","blue","10cf9b","13b934","647f1f","81431E","ef3038","2b4ab1","715438","814994","purple","ddde1e","c6721a","ea7c60","d154e9","ea5c61","998df0","27e33c","9bcb1a","edca77","a1f3b1",/*repeat*/"red","orange","yellow","34CA00","green","steelblue","blue","10cf9b","13b934","647f1f","81431E","ef3038","2b4ab1","715438","814994","purple","ddde1e","c6721a","ea7c60","d154e9","ea5c61","998df0","27e33c","9bcb1a","edca77","a1f3b1"];
+
+	var arc = d3.arc()
+		.outerRadius(radius - 10)
+		.innerRadius(0);
+
+	var labelArc = d3.arc()
+		.outerRadius(radius - 40)
+		.innerRadius(radius - 40);
+
+	var pie = d3.pie()
+		.sort(null)
+		.value(function(d) { return d; });
+
+	var svg = d3.select("#piechart").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	var g = svg.selectAll(".arc")
+		.data(pie(occurence_list))
+		.enter().append("g")
+		.attr("class", "arc");
+
+	g.append("path")
+		.attr("d", arc)
+		.style("opacity", 0.8)
+		.style("fill", function(d, i) { return color[i]; })
+		.on("mouseover", function() { d3.select(this).style("stroke-width", "2px").style("opacity", 1)})
+		.on("mouseout", function() { d3.select(this).style("stroke-width", "1px").style("opacity", 0.8)});
+			
+
+	g.append("text")
+		.attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+		.text(function(d) { return d.data; })
+		.attr("dy", "1em");
+	g.append("text")
+		.attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+		.text(function(d) { return label_list[d.index]; })
+		.attr("dy", "0em");
+
+</script>
 </div>
-
-
 
 </section>
 </div>
